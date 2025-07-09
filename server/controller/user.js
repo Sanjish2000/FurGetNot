07bf81cab546a0814cd6ee52564cf57ba3b63342
Sanjish Chouhan <sanjishchouhan@gmail.com>
2025-07-10 02:33:48 +0530

    png icon update

--- server/controller/user.js
+++ server/controller/user.js
@@ -106,7 +106,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // true on HTTPS
-      sameSite: "None", // CSRF protection
+      sameSite: "Lax", // CSRF protection
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
