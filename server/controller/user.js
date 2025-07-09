307957b7e3728421b0e5774998c04fb00b6b062a
Sanjish Chouhan <sanjishchouhan@gmail.com>
2025-07-10 02:28:30 +0530

    update user controller

--- server/controller/user.js
+++ server/controller/user.js
@@ -106,7 +106,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // true on HTTPS
-      sameSite: "Lax", // CSRF protection
+      sameSite: "None", // CSRF protection
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
