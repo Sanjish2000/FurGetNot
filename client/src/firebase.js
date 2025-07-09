f0a0432650149716584b68c188e938762a5e141c
Sanjish Chouhan <sanjishchouhan@gmail.com>
2025-07-09 22:09:50 +0530

    First commit: FurGetNot Web App

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbdINx6SefSuwfpjtGt4M0rywsMcxj3Dw",
-  authDomain: "furgetnot.firebaseapp.com",
-  projectId: "furgetnot",
-  storageBucket: "furgetnot.firebasestorage.app",
-  messagingSenderId: "412132078417",
-  appId: "1:412132078417:web:97f1944796487f89195970"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
