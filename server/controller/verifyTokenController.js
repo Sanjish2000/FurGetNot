a2fff576d09aef50bd4bba82e5f16e665f04c7c5
Sanjish Chouhan <sanjishchouhan@gmail.com>
2025-07-10 12:49:44 +0530

    firrbase

--- server/controller/verifyTokenController.js
+++ server/controller/verifyTokenController.js
@@ -5,10 +5,14 @@ export const verifyToken = (req, res) => {
     const token = req.cookies.token;
 
     if (!token) {
+      console.log("token not get");
       return res.status(401).json({
         success: false,
         message: "Token not found. Please login again.",
+        
+        
       });
+      
     }
 
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
