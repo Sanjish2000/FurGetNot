27e0570d039eb5d346afd6a0d36a0780bcc5a23b
Sanjish Chouhan <chouhansanjish@gmail.com>
2025-08-20 23:00:58 +0530

    time 10:58 bje

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use("/uploads", express.static("uploads"));
 
 app.use(
   cors({
-    origin: "https://fur-get-not-git-main-sanjishs-projects.vercel.app",
+    origin: "https://fur-get-not.vercel.app",
     credentials: true,
   })
 );
